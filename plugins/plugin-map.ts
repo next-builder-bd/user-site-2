8827582c1c011520f6f92790c0a43b28a5c57dd3
tuhinjamal2 <tuhinjamal2@gmail.com>
2025-12-29 23:39:10 +0600

    Update plugin-map.ts

--- plugins/plugin-map.ts
+++ plugins/plugin-map.ts
@@ -1,10 +1,8 @@
 // ⚠️ AUTO-GENERATED FILE — DO NOT EDIT
 import blog from "./blog/plugin";
-import shop-plugin from "./shop-plugin/plugin";
 import shopPlugin from "./shopPlugin/plugin";
 
 export const pluginMap = {
   blog,
-  shop-plugin,
   shopPlugin,
 };
